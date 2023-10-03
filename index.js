081fb29154644ebc1431675fd4e483c68e13d2cc
RM <rm@rmllcfs.com>
2023-10-03 15:55:06 -0400

    changed greeting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'there, all quit on the front'});
 });
 
 const PORT = process.env.PORT || 5000;
